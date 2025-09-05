6794eb4593ff35b1a2a8b191f2d43970a9352a9d
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-09-05 15:01:02 -0700

    Stopped using zip files

--- download.bat
+++ download.bat
@@ -1,4 +1,3 @@
-curl -L -o Python.zip https://github.com/s-rayyan/securly-python-bypass/raw/main/Python.zip
-powershell -command "Expand-Archive -Path Python.zip -DestinationPath %HOMEPATH%\Python -Force"
-rmdir %HOMEPATH%\Python1 /s /q
+curl -L -o Python.zip curl -L -o Python.zip https://github.com/s-rayyan/copper-python/archive/refs/heads/main.zip
+powershell -command "Expand-Archive -Path Python.zip -DestinationPath %HOMEPATH%\ -Force"
 del Python.zip
