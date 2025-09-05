6f9935c55ebdaf9b6b1b0cb5c09501dee07cf03c
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-09-04 20:18:16 -0700

    Fix Download.bat to work with Python.zip

--- download.bat
+++ download.bat
@@ -1,5 +1,4 @@
 curl -L -o Python.zip https://github.com/s-rayyan/securly-python-bypass/raw/main/Python.zip
-powershell -command "Expand-Archive -Path Python.zip -DestinationPath %HOMEPATH%\Python1 -Force"
-move %HOMEPATH%\Python1\securly-python-bypass-main %HOMEPATH%\Python
+powershell -command "Expand-Archive -Path Python.zip -DestinationPath %HOMEPATH%\Python -Force"
 rmdir %HOMEPATH%\Python1 /s /q
 del Python.zip
