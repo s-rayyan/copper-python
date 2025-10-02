99ed1a1d18285dafd7dfd60d873b34ea2b09fe84
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-10-02 16:32:48 -0700

    Bug fix

--- download.bat
+++ download.bat
@@ -2,5 +2,8 @@
 curl -L -o Python.zip curl -L -o Python.zip https://github.com/s-rayyan/copper-python/archive/refs/heads/main.zip
 powershell -command "Expand-Archive -Path Python.zip -DestinationPath %HOMEPATH%\ -Force"
 del Python.zip
-powershell -NoProfile -Command "& { $profile; function python { & `"$HOME\copper-python-main\python.ps1`" @args }; python %* }"
+powershell -NoProfile -Command ^
+"if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force }; ^
+Add-Content -Path $PROFILE -Value 'function python { & `"$HOME\copper-python-main\python.ps1`" @args }'"
+
 
