e5a4145d66e844f37633c8885d411011bff0e1df
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-10-03 14:04:59 -0700

    Update download.bat

--- download.bat
+++ download.bat
@@ -7,10 +7,11 @@ powershell -NoProfile -Command "Expand-Archive -Path 'Python.zip' -DestinationPa
 
 REM Delete archive
 del Python.zip
-
+mkdir %HOMEPATH%\Documents\WindowsPowerShell\
 REM Add python function to PowerShell profile if not already present
 powershell -NoProfile -Command ^
 "if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force }; ^
 if (-not (Select-String -Path $PROFILE -Pattern 'function python')) { ^
   Add-Content -Path $PROFILE -Value 'function python { & `$HOME\copper-python-main\python.ps1 @args }' ^
 }"
+
