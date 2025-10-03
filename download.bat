1f24308d974d3b2b92a91970bfdb62dd12e85eb2
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-10-03 12:55:03 -0700

    Update download.bat

--- download.bat
+++ download.bat
@@ -3,12 +3,14 @@ REM Download zip
 curl -L -o Python.zip https://github.com/s-rayyan/copper-python/archive/refs/heads/main.zip
 
 REM Extract to user home
-powershell -command "Expand-Archive -Path 'Python.zip' -DestinationPath $env:HOMEPATH -Force"
+powershell -NoProfile -Command "Expand-Archive -Path 'Python.zip' -DestinationPath $env:HOMEPATH -Force"
 
 REM Delete archive
 del Python.zip
 
-REM Add python function to PowerShell profile
+REM Add python function to PowerShell profile if not already present
 powershell -NoProfile -Command ^
 "if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force }; ^
-Add-Content -Path $PROFILE -Value 'function python { & `$HOME\copper-python-main\python.ps1 @args }'"
+if (-not (Select-String -Path $PROFILE -Pattern 'function python')) { ^
+  Add-Content -Path $PROFILE -Value 'function python { & `$HOME\copper-python-main\python.ps1 @args }' ^
+}"
