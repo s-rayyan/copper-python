c64b713bc7d1a665e7b46406f4b5cb8fd14eec5d
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2025-10-03 14:22:27 -0700

    Update download.bat

--- download.bat
+++ download.bat
@@ -4,7 +4,8 @@ curl -L -o Python.zip https://github.com/s-rayyan/copper-python/archive/refs/hea
 powershell -NoProfile -Command "Expand-Archive -Path 'Python.zip' -DestinationPath $env:HOMEPATH -Force"
 
 del Python.zip
-
+mkdir %HOME%\Documents\WindowsPowerShell
 powershell -NoProfile -Command ^
 "if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force }; ^
 Add-Content -Path $PROFILE -Value 'function python { & `$HOME\copper-python-main\python.ps1 @args }'"
+
